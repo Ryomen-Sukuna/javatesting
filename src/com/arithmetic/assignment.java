558fcdee0291b551c48234789364acaf9ed566ca
Ryomen-Sukuna <ryomensukuna@anomali.eu.org>
2022-06-10 17:16:08 +0700

    Example for assignment operator

---
+++ src/com/arithmetic/assignment.java
@@ -0,0 +1,37 @@
+package com.arithmetic;
+
+public class assignment {
+    public static void main(String[] args) {
+        // Declaration
+        int x;
+        int y;
+
+        // Fill the value
+        x = 20;
+        y = 25;
+
+        // Addition
+        y += x;
+
+        // Print to the screen
+        System.out.println("Addition : " + y);
+
+        // Substraction
+        y -= x;
+
+        // FPrint to the screen
+        System.out.println("Substraction : " + y);
+
+        // Division
+        y /= x;
+
+        // Print to the screen
+        System.out.println("Division : " + y);
+
+        // Modulus
+        y %= x;
+
+        // Print to the screen
+        System.out.println("Modulus : " + y);
+    }
+}
